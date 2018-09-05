60f7fa09c928cc9b0e5ad09ea81b0531491b82b0
suelliton <tonmelodicmetal@gmail.com>
2018-09-05 00:36:09 -0300

    second commit

--- app/src/main/java/com/example/suelliton/limi/AdicionarColaborador.java
+++ app/src/main/java/com/example/suelliton/limi/AdicionarColaborador.java
@@ -68,7 +68,6 @@ public class AdicionarColaborador extends AppCompatActivity {
                     }
                 }
             }
-
             @Override
             public void onCancelled(DatabaseError databaseError) {
 
